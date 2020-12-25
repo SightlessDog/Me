fc50e5a92478704163f4f117b0ff8cb5e4b937df
elyess.eleuch <ely.369@outlook.com>
2020-12-25 15:51:35 +0100

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: __dirname + '/build',
+    publicPath: '/',
+    filename: 'main.js',
+  },
+  devServer: {
+    contentBase: './build',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'eslint-loader'],
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin({ template: path.resolve('./index.html') })],
+};
